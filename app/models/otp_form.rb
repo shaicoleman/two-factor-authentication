fbe524610dfb62cb0afead4c59b9adb390a5bb0f
Shai Coleman <shai.coleman@cashanalytics.com>
2019-12-04 17:40:16 +0000

    Fix validation

--- app/models/otp_form.rb
+++ app/models/otp_form.rb
@@ -4,5 +4,5 @@ class OtpForm
   attr_accessor :otp_attempt
   attr_accessor :remember_me
 
-  validates :otp_attempt, presence: true, length: { is: 6 }, numericality: { only_integer: true }
+  validates :otp_attempt, presence: true, length: { is: 6 }, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
