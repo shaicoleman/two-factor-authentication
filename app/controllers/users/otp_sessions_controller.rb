17f78da47e978842c299babad00e5eda5576a247
Shai Coleman <shai.coleman@cashanalytics.com>
2019-12-04 16:14:44 +0000

    Record failed_otp_attempts

--- app/controllers/users/otp_sessions_controller.rb
+++ app/controllers/users/otp_sessions_controller.rb
@@ -16,14 +16,20 @@ class Users::OtpSessionsController < Devise::SessionsController
 
     self.resource = User.find_by(id: session[:otp_user_id])
     return redirect_to(new_user_session_path) unless self.resource.present?
+    return render 'devise/otp_sessions/new' unless @otp_form.valid?
 
     is_valid_otp = resource.validate_and_consume_otp!(@otp_form.otp_attempt)
-    @otp_form.errors.add(:otp_attempt, 'invalid code') unless is_valid_otp
-    return render 'devise/otp_sessions/new' unless @otp_form.valid?
+    unless is_valid_otp
+      @otp_form.errors.add(:otp_attempt, 'invalid code')
+      User.increment_counter(:failed_otp_attempts, self.resource.id)
+      return render 'devise/otp_sessions/new'
+    end
 
     sign_out
     sign_in(resource_name, resource)
+    self.resource.update_columns(failed_otp_attempts: 0)
     set_flash_message!(:notice, :signed_in)
+
 
     respond_with resource, location: after_sign_in_path_for(resource)
   end
