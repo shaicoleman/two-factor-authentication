7fcd9e0053e1fc56564916762c107698ee40c5d4
Shai Coleman <shai.coleman@cashanalytics.com>
2019-12-04 18:01:32 +0000

    Strip whitespace before validatoin

---
+++ app/models/active_model_form.rb
@@ -0,0 +1,4 @@
+class ActiveModelForm
+  include ActiveModel::Model
+  include ActiveModel::Validations::Callbacks
+end
--- app/models/otp_form.rb
+++ app/models/otp_form.rb
@@ -1,8 +1,13 @@
-class OtpForm
-  include ActiveModel::Model
-
+class OtpForm < ActiveModelForm
   attr_accessor :otp_attempt
   attr_accessor :remember_me
 
+  before_validation :strip_whitespace
   validates :otp_attempt, presence: true, length: { is: 6 }, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  private
+
+  def strip_whitespace
+    otp_attempt.remove!(/\s+/)
+  end
 end
