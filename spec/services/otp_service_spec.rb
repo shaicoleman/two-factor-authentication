e07dbd9fafe855f6d3dc866e26c362a74b6d9814
Shai Coleman <shai.coleman@cashanalytics.com>
2020-01-08 10:33:00 +0000

    Added spec comments

--- spec/services/otp_service_spec.rb
+++ spec/services/otp_service_spec.rb
@@ -22,34 +22,45 @@ RSpec.describe OtpService do
       now = Time.now.utc.to_i
       otp_secret = 'bseq4wbk7ycoeasjkxj6dd4njqz2zpfx'
       totp = ROTP::TOTP.new(otp_secret)
-      user = User.create!(email: 'test@test.com', password: 'secret', otp_secret: otp_secret)
+      user = User.create!(email: 'test@test.com', password: 'secret', otp_secret: otp_secret, otp_grace_period_started_at: 1.day.ago)
 
       # Allowed to use codes from the previous timestep
       expect(OtpService.attempt_otp(user: user, otp_attempt: totp.at(now - 30), at: now)).to eq(:success)
+
+      # Reset failed attempts counter on successful login
       expect(user.reload.otp_failed_attempts).to eq(0)
+
+      # Reset grace period on successful login
+      expect(user.otp_grace_period_started_at).to eq(nil)
 
       # Allowed to use codes from the current timestep
       expect(OtpService.attempt_otp(user: user, otp_attempt: totp.at(now), at: now)).to eq(:success)
 
-      # not allowed to reuse codes
+      # Not allowed to reuse codes
       expect(OtpService.attempt_otp(user: user, otp_attempt: totp.at(now), at: now)).to eq(I18n.t('auth.otp_sessions.otp_code_already_used_error'))
+
+      # Do not increment failed attempts counter when reusing code
       expect(user.reload.otp_failed_attempts).to eq(0)
 
-      # not allowed to use codes older than the last successful timestep
+      # Not allowed to use codes older than the last successful timestep
       expect(OtpService.attempt_otp(user: user, otp_attempt: totp.at(now - 30), at: now)).to eq(I18n.t('auth.otp_sessions.otp_code_already_used_error'))
 
-      # not allowed to use code from 2 timesteps ago
+      # Not allowed to use code from 2 timesteps ago
       expect(OtpService.attempt_otp(user: user, otp_attempt: totp.at(now - 60), at: now)).to eq(I18n.t('errors.messages.invalid'))
 
-      # not allowed to use code from the future
+      # Not allowed to use code from the future
       expect(OtpService.attempt_otp(user: user, otp_attempt: totp.at(now + 30), at: now)).to eq(I18n.t('errors.messages.invalid'))
+
+      # Increment the failed attempt counter on each failure
       expect(user.reload.otp_failed_attempts).to eq(2)
 
-      # when ignore_failed: true, do not increment otp_failed_attempts
+      # When ignore_failed: true, it should still return an error
       expect(OtpService.attempt_otp(user: user, otp_attempt: totp.at(now + 30), at: now, ignore_failed: true)).to eq(I18n.t('errors.messages.invalid'))
+
+      # When ignore_failed: true, it shouldn't increment the failed attempts counter
       expect(user.reload.otp_failed_attempts).to eq(2)
 
-      # not allowed when number of attempts exceeded, even if correct
+      # Not allowed when number of attempts exceeded, even if correct
       user.update!(otp_failed_attempts: 999, otp_consumed_timestep: nil)
       expect(OtpService.attempt_otp(user: user, otp_attempt: totp.at(now), at: now)).to eq(I18n.t('auth.too_many_failed_attempts'))
     end
